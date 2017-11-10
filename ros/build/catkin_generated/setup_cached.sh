2270348df54c12b5fb681e22b3592f7dd338376e
naoki.akai <akai@coi.nagoya-u.ac.jp>
2017-11-10 15:39:05 +0900

    initial commit

---
+++ ros/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/akai/Dropbox/work/AutoNavi/ros/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/akai/Dropbox/Research/cmob_auto/ros/devel/include:/opt/ros/indigo/include:/usr/local/cuda-7.5/include"
+export LD_LIBRARY_PATH="/home/akai/Dropbox/Research/cmob_auto/ros/devel/lib:/opt/ros/indigo/lib:/usr/local/cuda-7.5/lib64:/usr/local/cuda-7.5/lib:/usr/local/cuda/lib64"
+export PWD="/home/akai/Dropbox/work/AutoNavi/ros/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/akai/Dropbox/work/AutoNavi/ros/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/akai/Dropbox/work/AutoNavi/ros/src:$ROS_PACKAGE_PATH"
